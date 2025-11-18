8d0f95fa9dfd3889520fcc46e7481e07d88f2d74
samuel.monsalve <samuelalejomonsalve@gmail.com>
2025-11-18 12:32:37 -0500

    coors

--- src/main/java/departamentoDefensa/config/SecurityConfig.java
+++ src/main/java/departamentoDefensa/config/SecurityConfig.java
@@ -34,11 +34,15 @@ public class SecurityConfig {
      @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+         configuration.setAllowedOrigins(Arrays.asList(
+                 "http://localhost:4200",
+                 "https://tu-frontend-produccion.com" // si lo tienes
+         ));
+
+         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+        configuration.setExposedHeaders(List.of("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
